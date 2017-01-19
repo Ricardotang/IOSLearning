d2dbb408a2a3d14290446b5a695bc9aecdedf7d4
Ricardotang <dakeshuide@gmail.com>
2017-01-19 17:04:25 +0800

    add project named Memo for practice

---
+++ Memo/Memo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Memo
+//
+//  Created by mac on 2017/1/19.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Memo/Memo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Memo
+//
+//  Created by mac on 2017/1/19.
+//  Copyright © 2017年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
